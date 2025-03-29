6a2029bd69adf01478513e8801bb9ab0e8245271
Adryan Claro <adryan.contatoprofissional@gmail.com>
2025-03-29 16:02:32 -0300

    feat(entities): criado entidades do projeto

---
+++ src/main/java/com/lexlabor/entities/LeiTrabalhista.java
@@ -0,0 +1,24 @@
+import lombok.Data;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "LEITRABALHISTA")
+public class LeiTrabalhista {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LEI_ID")
+    private Long id;
+
+    @Column(name = "LEI_TITULO", nullable = false)
+    private String titulo;
+
+    @Column(name = "LEI_TEXTO", nullable = false, columnDefinition = "TEXT")
+    private String texto;
+
+    @ManyToMany(mappedBy = "leisFavoritas")
+    private List<Usuario> usuariosFavoritos;
+
+}
