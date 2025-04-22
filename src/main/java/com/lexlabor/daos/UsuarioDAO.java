8127b7a1decfb1bb7e99223505b7cd1cb19de8f8
MrClaro <adryan.contatoprofissional@gmail.com>
2025-04-22 16:29:08 -0300

    feat(daos): criado dao para realizar as transações com o banco de dados dos usuarios

---
+++ src/main/java/com/lexlabor/daos/UsuarioDAO.java
@@ -0,0 +1,69 @@
+package com.lexlabor.daos;
+
+import com.lexlabor.entities.Usuario;
+import com.lexlabor.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import java.util.List;
+
+public class UsuarioDAO {
+
+    public void save(Usuario usuario) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(usuario);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Usuario usuario) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(usuario);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Long id) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Usuario usuario = session.get(Usuario.class, id);
+            if (usuario != null) {
+                session.delete(usuario);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    public Usuario getById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Usuario.class, id);
+        }
+    }
+
+    public List<Usuario> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Usuario", Usuario.class).list();
+        }
+    }
+
+    public Usuario getByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Usuario where email = :email", Usuario.class)
+                    .setParameter("email", email)
+                    .uniqueResult();
+        }
+    }
+}
